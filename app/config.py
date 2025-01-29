fbdcbc91e466bb027b82035870b2c7f2815b3e33
Imad Zalmat <imaden.zalmat@gmail.com>
2025-01-29 18:43:52 +0100

    Fix email issue

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    EMAIL = os.getenv("EMAIL", "zalmat.online.courses@example.com")
+    EMAIL = os.getenv("EMAIL", "zalmat.online.courses@gmail.com")
     GITHUB_URL = os.getenv("GITHUB_URL", "https://github.com/imadoszmt/Hng12_Backend_Stage0_API-Project")
